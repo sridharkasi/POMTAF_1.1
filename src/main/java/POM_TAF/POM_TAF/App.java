91b77e207d9d906ae7c85288e5755a9af5d9be9e
Sridharan Kasinathan <Skasinathan@asiapac.globalcsc.net>
2017-07-18 19:50:42 +0530

    first commit

---
+++ src/main/java/POM_TAF/POM_TAF/App.java
@@ -0,0 +1,13 @@
+package POM_TAF.POM_TAF;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
