9169e296e199341b6d374d098d3c49b022f97272
Govinda Rao <grao24@asiapac.globalcsc.net>
2017-07-19 18:27:41 +0530

    Checked out by Govind branch

--- src/test/java/tests/TC2_FacebookLogin.java
+++ src/test/java/tests/TC2_FacebookLogin.java
@@ -5,6 +5,7 @@ public class TC2_FacebookLogin {
 	public void loginfacebook(){
 		System.out.println("Welcome to facebook");
 		System.out.println("Edited by Govind");
+		System.out.println("Edited by Govind second time");
 		
 	}
 
